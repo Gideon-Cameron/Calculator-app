bf9e44c3cf6faaca86082cb2c26173b2c244fdfa
Gideon <gideonwork20@gmail.com>
2024-03-05 12:10:01 +0100

    My Solution

---
+++ README.md
@@ -0,0 +1,2 @@
+My solution to the calculator app.
+If anyone has any feedback, it would be apriceated.
---
+++ app.js
@@ -0,0 +1,30 @@
+function del() {
+    let value = document.getElementById('screen').value;
+    document.getElementById('screen').value = value.substr(0,value.length-1);
+    
+}
+
+let two = document.getElementById('two');
+two.addEventListener('click', function() {
+    let body = document.querySelector('body');
+    let toggle = document.getElementById('circle');
+    body.classList.add('active1');
+    body.classList.remove('active2');
+    toggle.style.left = "36%";
+})
+let one = document.getElementById('one');
+one.addEventListener('click', function() {
+    let body = document.querySelector('body');
+    let toggle = document.getElementById('circle');
+    body.classList.remove('active1');
+    body.classList.remove('active2');
+    toggle.style.left = "0";
+})
+let three = document.getElementById('three');
+three.addEventListener('click', function() {
+    let body = document.querySelector('body');
+    let toggle = document.getElementById('circle');
+    body.classList.add('active2');
+    body.classList.remove('active1');
+    toggle.style.left = "65%";
+})
